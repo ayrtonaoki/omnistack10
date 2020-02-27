20ca5e44698f12db0ab2c443637c1c4ecd6411ba
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-02-27 10:35:07 -0300

    Install Axios

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://ayrton:ayrton@cluster0-xgqu3.gcp.mongodb.net/database?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(2222);
