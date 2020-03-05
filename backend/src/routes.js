160206a43183f8b8df5e6ad47a9543145217be30
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-03-05 01:37:10 -0300

    Move code from routes to dev controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,8 @@
 const { Router } = require('express');
-const axios = require('axios');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-  const { github_username } = request.body;
-  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-  console.log(apiResponse.data);
-
-  return response.json({ message: 'Hello Omnistack' });
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
