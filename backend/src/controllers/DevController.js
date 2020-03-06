cb6e11668cb43ec41060a6f05fc58c4a556ec776
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-03-05 22:28:03 -0300

    Avoid duplicated devs creation

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,25 +1,31 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
+const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
-  async store (request, response) {
+  async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    const { name = login, avatar_url, bio } = apiResponse.data;
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    };
+    let dev = { github_username };
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      bio,
-      techs: techsArray,
-      location,
-    });
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+      const { name = login, avatar_url, bio } = apiResponse.data;
+      const techsArray = techs.split(",").map(tech => tech.trim());
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return response.json(dev);
   }
-}
+};
