1b0c690efeda0b452ca8884b835f26dc1166333d
Ayrton Aoki <ayrtonaoki@Ayrtons-MacBook-Air.local>
2020-03-09 23:57:46 -0300

    Improve code identation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://ayrton:ayrton@cluster0-xgqu3.gcp.mongodb.net/database?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(
+  "mongodb+srv://ayrton:ayrton@cluster0-xgqu3.gcp.mongodb.net/database?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
